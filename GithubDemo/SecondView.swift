ef3634ff3f217326eee35bdb9398e7dc2a600ed6
PeterZhu <zyxchelsea11@gmail.com>
2022-08-28 15:00:14 +0800

    Create a second view

---
+++ GithubDemo/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GithubDemo
+//
+//  Created by PeterZ on 2022/8/28.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("This is the second view")
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
