37709dfbfd6d6896ac782131886dad19ec2c8041
PeterZ <peter.zhu@yff.com>
2022-08-29 13:52:54 +0800

    看看那个鬼东西删了没

---
+++ GithubDemo/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  GithubDemo
+//
+//  Created by admin on 2022/8/29.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
