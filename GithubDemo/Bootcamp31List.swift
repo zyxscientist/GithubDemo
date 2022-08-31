ae2fdec977c6802872627660b4b60a358b7024f6
PeterZhu <zyxchelsea11@gmail.com>
2022-08-31 17:24:05 +0800

    添加滑动删除操作与排序修改操作

--- GithubDemo/Bootcamp31List.swift
+++ GithubDemo/Bootcamp31List.swift
@@ -15,7 +15,7 @@ struct Player: Identifiable {
 
 struct Bootcamp31List: View {
     
-    var player: [Player] = [
+    @State var player: [Player] = [
         Player(name: "Jack Peter Grealish", club: "Manchester City Football Club"),
         Player(name: "Kevin De Bruyne", club: "Manchester City Football Club"),
         Player(name: "Phil Foden", club: "Manchester City Football Club"),
@@ -24,20 +24,48 @@ struct Bootcamp31List: View {
         ]
     
     var body: some View {
-        List {
-                ForEach(player) { player in
-                    VStack(alignment: .leading){
-                        Text(player.name)
-                            .font(.system(size: 16, weight: .semibold, design: .rounded))
-                            .foregroundColor(.black)
-                        Text(player.club)
-                            .font(.system(size: 13, weight: .regular, design: .rounded))
-                            .foregroundColor(.gray)
+        NavigationView {
+            List {
+                    ForEach(player) { player in
+                        VStack(alignment: .leading){
+                            Text(player.name)
+                                .font(.system(size: 16, weight: .semibold, design: .rounded))
+                                .foregroundColor(.black)
+                            Text(player.club)
+                                .font(.system(size: 13, weight: .regular, design: .rounded))
+                                .foregroundColor(.gray)
+                        }
                     }
+                    .onMove(perform: { fromIndex, toIndex in
+                        moveItem(fromIndex: fromIndex, toIndex: toIndex)
+                    })
+                    .onDelete { indexSet in
+                        delete(indexSet: indexSet)
+                        
                 }
-            }.listStyle(PlainListStyle())
+            }.toolbar{
+                ToolbarItemGroup(placement: .navigationBarLeading){
+                    EditButton()
+                }
+                
+                ToolbarItemGroup(placement: .navigationBarTrailing){
+                    // MARK: ADD BTN
+                }
+            }
+                .navigationTitle("MFC")
+                .listStyle(DefaultListStyle())
+            }
+        }
+    
+        func delete(indexSet: IndexSet){
+            player.remove(atOffsets: indexSet)
+        }
+    
+        func moveItem(fromIndex: IndexSet, toIndex: Int){
+                player.move(fromOffsets: fromIndex, toOffset: toIndex)
         }
     }
+
 
 struct Bootcamp31List_Previews: PreviewProvider {
     static var previews: some View {
