e4f26328d62a36e9e61b43476d7d388a7f2d0a2a
PeterZhu <zyxchelsea11@gmail.com>
2022-08-31 21:37:32 +0800

    准备好所有东西，可以开始搞EnviornmentObject了

--- GithubDemo/BootcampEnvironmentObject.swift
+++ GithubDemo/BootcampEnvironmentObject.swift
@@ -12,19 +12,72 @@ struct DeviceType: Identifiable {
     var name: String
 }
 
+class DeviceViewModel:ObservableObject {
+    
+    @Published var deviceViewModel: [DeviceType] = []
+    
+    init(){
+        deviceViewModel.append(contentsOf: [DeviceType(name: "iPhone")])
+        deviceViewModel.append(contentsOf: [DeviceType(name: "Mac")])
+        deviceViewModel.append(contentsOf: [DeviceType(name: "Airpod Pro")])
+    }
+
+}
+
 struct BootcampEnvironmentObject: View {
     
-    @State var device: [DeviceType] = [DeviceType(name: "iPhone"),DeviceType(name: "iPad"),DeviceType(name: "MacBook")]
+    @StateObject var device: DeviceViewModel = DeviceViewModel()
     
     var body: some View {
-        List{
-            ForEach(device) { device in
-                Text(device.name)
-                    .font(.system(size: 16, weight: .semibold, design: .rounded))
-            }
+        NavigationView {
+            List{
+                ForEach(device.deviceViewModel) { device in
+                    NavigationLink {
+                        SecondPage(deviceName: device.name, deviceInSecondPage: self.device)
+                    } label: {
+                        Text(device.name)
+                            .font(.system(size: 16, weight: .semibold, design: .rounded))
+                    }
+
+                    }
+            }.navigationTitle("Device List")
         }
     }
 }
+
+struct SecondPage: View{
+    
+    let deviceName: String
+    @ObservedObject var deviceInSecondPage: DeviceViewModel
+    
+    var body: some View{
+        NavigationLink {
+            ThirdPage(viewModel: deviceInSecondPage)
+        } label: {
+            Text(deviceName)
+                .font(.system(size: 36, weight: .semibold, design: .rounded))
+                .foregroundColor(.white)
+                .padding(.all, 20)
+                .background(.blue)
+                .cornerRadius(40)
+        }
+    }
+    
+}
+
+struct ThirdPage: View{
+    
+    @ObservedObject var viewModel: DeviceViewModel
+    
+    var body: some View{
+        ForEach(viewModel.deviceViewModel) { item in
+            Text(item.name)
+                .padding(19)
+        }
+    }
+    
+}
+
 
 struct BootcampEnvironmentObject_Previews: PreviewProvider {
     static var previews: some View {
