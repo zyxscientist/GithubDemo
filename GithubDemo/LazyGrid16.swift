f185de22c76a0a8bf59d0b78e778a3faf11898d1
PeterZhu <zyxchelsea11@gmail.com>
2022-09-06 15:28:30 +0800

    LazyGrid入门完成

--- GithubDemo/LazyGrid16.swift
+++ GithubDemo/LazyGrid16.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct LazyGrid16: View {
+    
+    let gridItem: [GridItem] = [
+        GridItem(.flexible(), spacing: nil, alignment: nil), // 代表一竖
+        GridItem(.flexible(), spacing: nil, alignment: nil),
+        GridItem(.flexible(), spacing: nil, alignment: nil),
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVGrid(columns: gridItem,
+                      alignment: .center,
+                      spacing: 6,
+                      pinnedViews: [.sectionHeaders]){
+                Section(header:
+                            Text("Section 1")
+                                .foregroundColor(.white)
+                                .font(.system(.title, design: .rounded))
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 15)
+                                .padding(.vertical, 5)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .background(.black)
+                                .clipShape(RoundedRectangle(cornerRadius: 10.0, style: .continuous))
+                                .padding(.horizontal, 15)
+                            )
+                    {
+                    ForEach(0..<50) { item in
+                        Rectangle()
+                            .frame(height: 150)
+                            .foregroundColor(.pink)
+                    }
+                }
+            }
+        }
     }
 }
 
